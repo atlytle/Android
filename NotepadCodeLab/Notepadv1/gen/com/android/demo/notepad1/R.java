56d6bf8c413c6cf3fc413733868818d4dfbaf990
Andrew Lytle <atlytle+github@gmail.com>
2011-08-18 19:30:44 +0100

    Step 9.

--- NotepadCodeLab/Notepadv1/gen/com/android/demo/notepad1/R.java
+++ NotepadCodeLab/Notepadv1/gen/com/android/demo/notepad1/R.java
@@ -22,6 +22,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int menu_insert=0x7f040002;
         public static final int no_notes=0x7f040001;
     }
 }
